1e22474ca3552dc1e3dfdd4bc10ed91cb0b5d7a4
Tim-Jonas Schwarz <tmjssz@gmail.com>
2023-08-14 20:04:14 +0200

    Make listing field required for create project endpoint

--- cozero-backend/src/main.ts
+++ cozero-backend/src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3001);
 
   if (module.hot) {
--- cozero-backend/src/projects/dto/create-project.dto.ts
+++ cozero-backend/src/projects/dto/create-project.dto.ts
@@ -1,7 +1,11 @@
+import { ArrayNotEmpty } from 'class-validator';
+
 export class CreateProjectDto {
   name: string;
   description: string;
   co2EstimateReduction: number[];
   owner: string;
+  
+  @ArrayNotEmpty()
   listing: string[];
 }
